380fb3ac2d4186ec2a6b937380173cda94c7535f
AargusAtul <atuljaiswal.se@gmail.com>
2023-08-01 17:59:31 +0530

    adding footer and dropdown

--- src/Components/BasicBody/Footer.jsx
+++ src/Components/BasicBody/Footer.jsx
@@ -1,9 +1,151 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./BasicBody.css";
+import { Link } from "react-router-dom";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { TbMailFilled } from "react-icons/tb";
+import { BsGlobe2 } from "react-icons/bs";
+import { FiSearch } from "react-icons/fi";
+import logo from "../../Images/logo.svg";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+  FaWhatsapp,
+} from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div className='Footer'>Footer</div>
-  )
-}
+    <footer className="w-full h-fit py-5 bg-[#002C78] text-white md:place-content-center ">
+      {/* ------------------------footer just above------------------------ */}
+      <div className="FooterContact hidden md:flex justify-between place-items-center content-center items-center mx-[8rem] lg:px-[5.688rem] px-[2rem] py-[3rem] mt-[-5.5rem] ">
+        <h1 className=" text-2xl lg:text-4xl font-semibold">
+          Let's Start Conversation
+        </h1>
+        <button className="px-[1.5rem] py-3 text-xl font-bold bg-[#002C78] ">
+          Contact Us
+        </button>
+      </div>
+      {/* ------------------------footer navbar------------------------ */}
+      <div className="hidden md:block px-[6.25rem] pt-[3.62rem] pb-[28px] border-b border-white">
+        <ul className="flex justify-between ">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/solutions">solutions</Link>
+          </li>
+          <li>
+            <Link to="/partners">Partners</Link>
+          </li>
+          <li>
+            <Link to="/Careers">Careers</Link>
+          </li>
+          <li>
+            <Link to="/aboutus">About Us</Link>
+          </li>
+        </ul>
+      </div>
+      {/* -------------------------------Footer items------------------------------ */}
+      <div className="px-[1.25rem]  lg:px-[6.25rem]     gap-10  mt-[2.813rem] justify-between  grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+        <div className=" ">
+          {/* ----------------------------SErvices--------------------------------- */}
+          <h5 className="text-xl font-bold mb-[1.25rem]">Services</h5>
+          <ul className="gap-y-[5rem]">
+            <li>Conslting</li>
+            <li>Data Migration</li>
+            <li>Web Development</li>
+            <li>App Development</li>
+            <li>IT Infrastructure</li>
+            <li>Automation</li>
+            <li>Training</li>
+            <li>Tally Support</li>
+            <li>Managed Services</li>
+          </ul>
+        </div>
+        {/* ----------------------------Address--------------------------------- */}
 
-export default Footer
+        <div className=" ">
+          <h5 className="text-xl font-bold mb-[1.25rem]">Address</h5>
+          <ul className="gap-y-[5rem]">
+            <li>
+              <span className="font-bold">Regd. off. :</span> S-3, Anupam
+              PlazaOpposite Azad ApartmentsSri Aurbindo MargHauz KhasNew Delhi -
+              110016.
+            </li>
+            <li>
+              <span className="font-bold">Corp. Off. :</span> 605, Tower A,The
+              I-Thum, Plot No.A-40,Sector-62, Noida (U.P.)-201301
+            </li>
+          </ul>
+        </div>
+        {/* ----------------------------Contactus--------------------------------- */}
+
+        <div className="">
+          <h5 className="text-xl font-bold mb-[1.25rem]">Contact Us</h5>
+          <ul className="gap-y-[5rem] ">
+            <li className="flex items-center gap-[10px] mb-[15px]">
+              <BsFillTelephoneFill />
+              +91-11-41072677
+            </li>
+            <li className="flex items-center gap-[10px] mb-[15px]">
+              <TbMailFilled />
+              mail@aargusinfotec.com
+            </li>
+            <li className="flex items-center gap-[10px] mb-[15px]">
+              <BsGlobe2 />
+              www.aargusinfotec.com
+            </li>
+            {/* ----------------------------Search bar--------------------------------- */}
+
+            <div className="w-[14rem] h-[40px] bg-white rounded-md flex mt-[80px] ">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="text-lg text-black px-[19px] outline-none  w-[11rem] h-full rounded-md"
+              />
+              <div className="w-[3rem]  border-l-2 border-[#9F9797] place-content-center px-1 py-[.1rem]">
+                <button>
+                  <FiSearch className="text-[#9F9797] text-[2.25rem] " />
+                </button>
+              </div>
+            </div>
+          </ul>
+        </div>
+      </div>
+      {/* ------------------------------Copyright----------------------- */}
+      <div className="  py-[26px] mx-[1.25rem] md:mx-[6.25rem]  mt-[2.75rem] place-items-center justify-around items-center  gap-9 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+        <div className="">
+          <img src={logo} className="" alt="logo" />
+        </div>
+        <div className=" text-sm">
+          © 2023 All rights reserved by Aargus Infotec Pvt Ltd
+        </div>
+        <div className="	">
+          <ul className="flex gap-5 text-2xl ">
+            <li className="hover:text-[#1A99D4]">
+              <FaWhatsapp />
+            </li>
+            <li className="hover:text-[#1A99D4]">
+              <FaLinkedinIn />
+            </li>
+            <li className="hover:text-[#1A99D4]">
+              <FaTwitter />
+            </li>
+            <li className="hover:text-[#1A99D4]">
+              <FaInstagram />
+            </li>
+            <li className="hover:text-[#1A99D4]">
+              <FaFacebookF />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,10 +1,89 @@
-import React from 'react'
-import './Hero.css'
+import React, { useState, useRef, useEffect } from 'react';
+import './Hero.css';
+import list from '../BasicBody/list.json';
+import { BsChevronDown } from 'react-icons/bs';
+import { FaAngleUp } from 'react-icons/fa';
+
+const SubMenuItem = ({ item }) => {
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+
+  const toggleSubMenu = () => {
+    setIsSubMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <div key={item.title} className="relative">
+      <div
+        onClick={toggleSubMenu}
+        className={`${
+          isSubMenuOpen ? 'bg-blue-400 text-white' : 'bg-blue-300 text-black'
+        } p-4 w-full flex items-center justify-between font-bold tracking-wide border active:border-black duration-300`}
+      >
+        {item.title}
+        {isSubMenuOpen ? <FaAngleUp className="h-8" /> : <BsChevronDown className="h-8 text-black" />}
+      </div>
+      {isSubMenuOpen && (
+        <div className="bg-blue-300 absolute w-full top-0 left-full">
+          {item.children.map((nestedItem, index) => (
+            <div key={index} className="flex w-full justify-between p-3 hover:bg-blue-400">
+              <h1>{nestedItem.title}</h1>
+              <h1>{nestedItem.icon}</h1>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 const Hero = () => {
-  return (
-    <div className='Hero'>Hero</div>
-  )
-}
+  const dropdownRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Hero
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
+  return (
+    <div className="relative">
+      <span className="Hero text-orange-400 text-4xl">Hero</span>
+      <div
+        ref={dropdownRef}
+        className="relative flex flex-col items-center w-[340px] h-[340px]"
+      >
+        <button
+          onClick={() => setIsOpen((prev) => !prev)}
+          className={`${
+            isOpen ? 'bg-blue-400 text-white' : 'bg-blue-300 text-black'
+          } p-4 w-full flex items-center justify-between font-bold tracking-wide border active:border-black duration-300`}
+        >
+          Dropdown
+          {isOpen ? (
+            <FaAngleUp className="h-8" />
+          ) : (
+            <BsChevronDown className="h-8 text-black" />
+          )}
+        </button>
+        {isOpen && (
+          <div className="bg-blue-300 absolute w-full top-0 left-full">
+            {list.map((item) => (
+              <SubMenuItem key={item.title} item={item} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
